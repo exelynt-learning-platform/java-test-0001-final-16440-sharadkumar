c1c00d332ace53bfa545bd2dd8dddda4b0b97491
Sharad Kumar Yadav <sharad.dac@gmail.com>
2026-03-14 12:48:05 +0530

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,50 @@
+class java1 {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+
+            // leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // first star
+            System.out.print("*");
+
+            // inner spaces
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--) {
+
+            // leading spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // first star
+            System.out.print("*");
+
+            // inner spaces
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
